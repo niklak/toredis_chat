c38ce8ae896c6c6daa03f8d0278934a19e9ac773
Nikolay Gumanov <morgenpurple@gmail.com>
2016-01-31 20:49:09 +0200

    add semantic ui templates

--- conf.py
+++ conf.py
@@ -2,7 +2,7 @@ import os
 
 settings = {
     'cookie_secret': '__TODO:_GENERATE_YOUR_OWN_RANDOM_VALUE_HERE__',
-    'template_path': os.path.join(os.path.dirname(__file__), 'templates'),
+    'template_path': os.path.join(os.path.dirname(__file__), 'templates/semantic'),
     'static_path': os.path.join(os.path.dirname(__file__), 'static'),
     'login_url': '/login',
     'xsrf_cookies': True,
